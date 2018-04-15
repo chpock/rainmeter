07aae7310f4636f8268147319fb5647cf0af3548
Konstantin Kushnir <chpock@gmail.com>
2018-04-15 21:36:47 +0300

    Code cleanup + support zero values for the params
    Removed the code that tracks is intended to insert new sections by the
    command "@include" after the current section being processed. It became
    unnecessary, because the sections are now inserted during their
    processing.
    
    Fixed log messages.
    
    Added the ability to specify zero values for the first and last
    parameters.

--- Library/ConfigParser.h
+++ Library/ConfigParser.h
@@ -156,7 +156,6 @@ private:
 
 	std::unordered_set<std::wstring> m_FoundSections;
 	std::list<std::wstring> m_ListVariables;
-	std::list<std::wstring>::const_iterator m_SectionInsertPos;
 
 	std::unordered_map<std::wstring, std::wstring> m_BuiltInVariables;
 	std::unordered_map<std::wstring, std::wstring> m_Variables;
